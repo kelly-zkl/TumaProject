2eeb588c4e743131c14fbdbb1e60d3acff8b519c
kelly <1457242108@qq.com>
2019-12-09 18:55:34 +0800

    翻码工具布局修改

--- src/components/approval/RevTool.vue
+++ src/components/approval/RevTool.vue
@@ -3,26 +3,32 @@
     <section class="content">
       <div v-bind:style="'height:'+tableHeight+'px'">
         <el-form>
-          <el-form-item style="margin-bottom: 10px">
-            <el-input size="medium" placeholder="IMSI" :maxlength="15" style="width: 300px"
-                      v-model="input_imsi" @change="imsiChange"></el-input>
-            <el-button type="primary" size="medium" class="input-btn" :loading="imsiLoading"
-                       @click="revImsi()">IMSI翻手机号
-            </el-button>
-          </el-form-item>
-          <el-form-item style="margin-bottom: 10px" v-show="rev_phone">
-            <pre>{{rev_phone}}</pre>
-          </el-form-item>
-          <el-form-item style="margin-bottom: 10px">
-            <el-input size="medium" placeholder="手机号码" :maxlength="11" style="width: 300px"
-                      v-model="input_phone" @change="phoneChange"></el-input>
-            <el-button type="primary" size="medium" class="input-btn" :loading="phoneLoading"
-                       @click="revPhone()">手机号翻IMSI
-            </el-button>
-          </el-form-item>
-          <el-form-item style="margin-bottom: 10px" v-show="rev_imsi">
-            <pre>{{rev_imsi}}</pre>
-          </el-form-item>
+          <el-row :gutter="30">
+            <el-col :span="12">
+              <el-form-item style="margin-bottom: 10px;text-align: left">
+                <el-input size="medium" placeholder="IMSI" :maxlength="15" style="width: 300px"
+                          v-model="input_imsi" @change="imsiChange"></el-input>
+                <el-button type="primary" size="medium" class="input-btn" :loading="imsiLoading"
+                           @click="revImsi()">IMSI翻手机号
+                </el-button>
+              </el-form-item>
+              <el-form-item style="margin-bottom: 10px;text-align: left" v-show="rev_phone">
+                <pre class="pre-content">{{rev_phone}}</pre>
+              </el-form-item>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item style="margin-bottom: 10px;text-align: left">
+                <el-input size="medium" placeholder="手机号码" :maxlength="11" style="width: 300px"
+                          v-model="input_phone" @change="phoneChange"></el-input>
+                <el-button type="primary" size="medium" class="input-btn" :loading="phoneLoading"
+                           @click="revPhone()">手机号翻IMSI
+                </el-button>
+              </el-form-item>
+              <el-form-item style="margin-bottom: 10px;text-align: left" v-show="rev_imsi">
+                <pre class="pre-content">{{rev_imsi}}</pre>
+              </el-form-item>
+            </el-col>
+          </el-row>
         </el-form>
       </div>
     </section>
@@ -86,5 +92,12 @@
   .input-btn {
     padding: 8px 12px;
     margin-left: 10px
+  }
+
+  .pre-content {
+    white-space: pre-wrap;
+    white-space: -moz-pre-wrap;
+    white-space: -o-pre-wrap;
+    word-wrap: break-word;
   }
 </style>
